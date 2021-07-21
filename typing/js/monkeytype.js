eea0f18e959ffa984dd2f4c6c8dd06a6f4f9ed83
Charles Xiong <22375724+S-Ex1t@users.noreply.github.com>
2021-07-22 01:46:53 +0800

    Site updated: 2021-07-22 01:46:52

--- typing/js/monkeytype.js
+++ typing/js/monkeytype.js
@@ -15194,12 +15194,12 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 var mappedRoutes = {
-  "/typing": "pageLoading",
-  "/typing/login": "pageLogin",
-  "/typing/settings": "pageSettings",
-  "/typing/about": "pageAbout",
-  "/typing/account": "pageAccount",
-  "/typing/verify": "pageTest"
+  "/": "pageLoading",
+  "/login": "pageLogin",
+  "/settings": "pageSettings",
+  "/about": "pageAbout",
+  "/account": "pageAccount",
+  "/verify": "pageTest"
 };
 
 function handleInitialPageClasses(pathname) {
